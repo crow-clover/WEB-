8740a36e1255f0c4867fa4d2a13c63abfa77dab6
crow-clover <150483402+crow-clover@users.noreply.github.com>
2024-05-30 08:29:05 +0900

    ポートフォリオサイト　途中経過

---
+++ profile/haguruma.js
@@ -0,0 +1,69 @@
+// glowAnimeにglowというクラス名を付ける定義
+function GlowAnimeControl() {
+  $('.glowAnime').each(function () {
+    var elemPos = $(this).offset().top -50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height;
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("glow");
+
+    }else {
+      $(this).removeClass("glow");
+    }
+  });
+}
+
+// ロード画面
+function time() {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
+
+function loadTime() {
+    // spanタグを追加する
+    var element = $(".glowAnime");
+    element.each(function () {
+      // コンソールログに以下２つの動きを出す！
+      var text = $(this).text();
+      var textbox = "";
+      text.split('').forEach(function (t, i) {
+        if (t !== "") {
+          if (i < 9) {
+            textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+          } else {
+            var n = i + 1;
+            textbox += '<span style="animation-delay:.' + n + 's;">' + t + '</span>';
+          }
+  
+        } else {
+          textbox += t;
+        }
+      });
+      $(this).html(textbox);
+    });
+}
+
+
+// const promise = new Promise(function (resolve, reject) {
+
+//   setTimeout(function() {
+//     time;
+//     resolve();
+//   }, 4000);
+// });
+
+// promise.then(function () {
+//   GlowAnimeControl;
+//   loadTime;
+//   console.log('こんにちは');
+// })
+
+
+
+// 画面が読み込まれた時に動かしたい場合の記述
+$(window).on('load', function () {
+  GlowAnimeControl();
+  setTimeout(time, 4000);
+  setTimeout(loadTime, 7000);
+}); 
+
